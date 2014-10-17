48c92a1652a86364d28567c0689e459e3baa14cb
andreakmargo08 <andreak_31@hotmail.com>
2014-10-17 12:52:41 -0500

    agregue plantillas 1

---
+++ centro1/public/index.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="es">
+<head>
+    <title>CENTRO DE INVESTIGACIONES--(FCBI) Universidad de los llanos</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=0" />
+    <!-- css -->
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="css/estilo_slider.css">
+    <!-- scripts -->
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+    <script type="text/javascript" src="js/texto-imagenes-slider.js"></script>
+
+</head>
+	<body>
+		<div class="body">		
+			<!-- Cabecera -->
+			<header class="capa-cabeza">
+				<!-- logo -->
+					<div id="logo-encabezado">
+						<img src="images/1.png" alt="" width="450" height="100"/>
+					</div>
+					<!-- Fecha -->
+					<div id="fecha-encabezado">
+						<p><?php include("fecha.php");?></p>
+					</div>
+					<!-- Redes sociales -->
+					<div id="redes-cabecera">
+						<ul> <!--poner target...abrir en otra pagina-->
+						  <li><a href="#" target="_blank" class="facebook"></a></li>
+					      <li><a href="#" target="_blank" class="twitter"></a></li>
+					      <li><a href="#" target="_blank" class="googlemas"></a></li>
+						</ul>
+					</div><!--Fin redes sociales cabecera-->					
+					<!-- Iniciar sesion -->
+					<div id="ingreso-encabezado">
+						<a href="#" title="Inicie sesion" class="popups-form-reload">Ingresar</a>
+					</div>
+			</header>
+			<!-- Menu de navegacion -->
+			<section id="menu">
+				<ul>
+				  <li class="nivel1"><a href="#" class="nivel1">Menu 1</a>
+					<ul>
+						<li><a href="#">Menu 1.1</a></li>
+					</ul>
+				<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+				  </li>
+				  <li class="nivel1"><a href="#" class="nivel1">Menu 2</a>
+				<!--[if lte IE 6]><a href="#" class="nivel1ie">Menu 2<table class="falsa"><tr><td><![endif]-->
+					<ul>
+						<li><a href="#">Menu 2.1</a></li>
+						<li><a href="#">Menu 2.2</a></li>
+						<li><a href="#">Menu 2.3</a></li>
+						<li><a href="#">Menu 2.4</a></li>
+					</ul>
+				<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+				</li>
+				  <li class="nivel1"><a href="#" class="nivel1">Menu 3</a>
+				<!--[if lte IE 6]><a href="#" class="nivel1ie">Menu 3<table class="falsa"><tr><td><![endif]-->
+					<ul>
+						<li><a href="#">Menu 3.1</a></li>
+						<li><a href="#">Menu 3.2</a></li>
+					</ul>
+				<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+				</li>
+				  <li class="nivel1"><a href="#" class="nivel1">Menu 4</a>
+				<!--[if lte IE 6]><a href="#" class="nivel1ie">Menu 4<table class="falsa"><tr><td><![endif]-->
+					<ul>
+						<li><a href="#">Menu 4.1</a></li>
+						<li><a href="#">Menu 4.2</a></li>
+						<li><a href="#">Menu 4.3</a></li>
+					</ul>
+				<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+				</li>
+				  <li class="nivel1"><a href="#" class="nivel1">Menu 5</a>
+				<!--[if lte IE 6]><a href="#" class="nivel1ie">Menu 5<table class="falsa"><tr><td><![endif]-->
+					<ul>
+						<li><a href="#">Menu 5.1</a></li>
+						<li><a href="#">Menu 5.2</a></li>
+					</ul>
+				<!--[if lte IE 6]></td></tr></table></a><![endif]-->
+				</li>
+				</ul>
+			</section>
+			<div class="cl">&nbsp;</div>
+			<!--fin menu de navegacion-->
+			<!-- Slider -->
+			<div id="slider1">
+		      	<div class="wrap">
+		        	<div id="slide-holder">
+		          		<div id="slide-img">
+			                <a href=""><img id="slide-img-1" src="images/slider-fotos/2.png" class="slide" alt="" /></a>
+			                <a href=""><img id="slide-img-2" src="images/slider-fotos/2.png" class="slide" alt="" /></a>
+			                <a href=""><img id="slide-img-3" src="images/slider-fotos/2.png" class="slide" alt="" /></a>
+			                <a href=""><img id="slide-img-4" src="images/slider-fotos/2.png" class="slide" alt="" /></a>
+			                <a href=""><img id="slide-img-5" src="images/slider-fotos/1.jpg" class="slide" alt="" /></a>
+			                <a href=""><img id="slide-img-6" src="images/slider-fotos/2.png" class="slide" alt="" /></a>
+			                <a href=""><img id="slide-img-7" src="images/slider-fotos/1.jpg" class="slide" alt="" /></a> 
+		            	</div>
+	            		<!--contenido del slider -->
+	            		<div class="botones-slider">
+		            		<div id="slide-controls">
+		            			<p id="slide-nav"></p>
+		            		</div>
+	            		</div>
+            			<div id="slide-controls1">
+        					<div id="cuadro">
+        						<p id="slide-client" class="text"><span></span></p>
+        					</div>
+		          		</div> 
+		        	</div>
+		    	</div>
+		    </div>	
+		    <div class="cl">&nbsp;</div>
+			<!-- Secciones-->	
+			<div class="content">
+			    <div class="content_resize">
+			        <div class="mainbar">
+			        	<div class="article">
+				         	<h2><span>Template License</span></h2>
+				          	<div class="clr"></div>
+				          	<p>Posted on 18. Sep, 2015 by Sara in Filed under templates, internet, with Comments 18</p>
+				          	<img src="images/img_1.jpg" width="202" height="147" alt="" class="fl" /> This is a free CSS website template by AtomicWebsiteTemplates.com. This work is distributed under the <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>, which means that you are free to use it for any personal or commercial purpose provided you credit me in the form of a link back to AtomicWebsiteTemplates.com.
+				          	<p>Maecenas dignissim mauris in arcu congue tincidunt. Vestibulum elit nunc, accumsan vitae faucibus vel, scelerisque a quam. Aenean at metus id elit bibendum faucibus. Curabitur ultrices ante nec neque consectetur a aliquet libero lobortis. Ut nibh sem, pellentesque in dictum eu, convallis blandit erat. Cras vehicula tellus nec purus sagittis id scelerisque risus congue. Quisque sed semper massa. Donec id lacus mauris, vitae pretium risus. Fusce sed tempor erat. </p>
+				          	<p><a href="#">Leer mas </a></p>
+			        	</div>
+				        <div class="article">
+				          	<h2><span>Aliquam Risus</span> Justo</h2>
+				          	<div class="cl"></div>
+				          	<p>Posted on 18. Sep, 2015 by Sara in Filed under templates, internet, with Comments 18</p>
+				          	<img src="images/img_1.jpg" width="202" height="147" alt="" class="fl" />
+				          	<p>Pellentesque posuere enim et ipsum dignissim convallis. Proin quis molestie mauris. Nunc eget quam at nulla tempus tincidunt quis a mi. Aliquam ornare turpis non tellus molestie imperdiet. Phasellus sit amet neque vitae purus venenatis hendrerit. Phasellus non mi ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse potenti. Aenean vel varius sapien. Etiam leo quam, sodales vel ullamcorper ut, viverra a risus.</p>
+				          	<p>Maecenas dignissim mauris in arcu congue tincidunt. Vestibulum elit nunc, accumsan vitae faucibus vel, scelerisque a quam. Aenean at metus id elit bibendum faucibus. Curabitur ultrices ante nec neque consectetur a aliquet libero lobortis. Ut nibh sem, pellentesque in dictum eu, convallis blandit erat. Cras vehicula tellus nec purus sagittis id scelerisque risus congue. Quisque sed semper massa. Donec id lacus mauris, vitae pretium risus. Fusce sed tempor erat. </p>
+				          	<p><a href="#">Leer mas </a></p>
+				        </div>
+			        </div>
+
+			      	<div class="sidebar">
+			        	<div class="gadget">
+							<div class="columna-abajo" id="cuadro">
+							<h2 class="star">Noticias</h2>
+						        <marquee  height="200px" direction="up" onmouseover="this.scrollAmount=0" onmouseout="this.scrollAmount=2.5" scrollamount="1.8" scrolldelay="0">
+						          	<ul class="list1 col3">
+						            <li>
+						              	<a href="#">titulo noticia</a>
+						                <span class="pnoticia-columna">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span><br><br>
+						                <img src="#" width="200px" height="3px"><br><br>
+						            </li>
+						            <li>
+						              	<a href="#">titulo noticia</a>
+						                <span class="pnoticia-columna">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span><br><br>
+						                <img src="#" width="200px" height="3px"><br><br>
+						            </li>
+						            <li>
+						              	<a href="#">titulo noticia</a>
+						                <span class="pnoticia-columna">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span><br><br>
+						                <img src="#" width="200px" height="3px"><br><br>
+						            </li>
+						            <li>
+						              <a href="#">titulo noticia</a>
+						                <span class="pnoticia-columna">xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</span><br><br>
+						                <img src="#" width="200px" height="3px"><br><br>
+						            </li>
+						          </ul>
+						        </marquee>
+							</div>
+			        	</div>
+
+			        	<div class="gadget">
+			        		<!--convocatorias-->
+			        		<h2 class="star"><span>Convocatorias</span></h2>
+			        		<ul class="list">
+						        <li>
+						        	<div class="date">
+							            <strong>01</strong>
+							            <span>2014</span>
+							            <em>Jun</em>
+						          	</div>
+						          	<div class="extra_wrapper">
+						            	<div class="col3"><a href="#">Convocatoria 1 </a></div>los pollitos dicen pio pio pio cuando tienen hambre cuando tienen frio, la gallina busca el maiz y el trigo les da la comida. 
+						          	</div>
+						        </li>
+						        <li>
+						        	<div class="date">
+							            <strong>01</strong>
+							            <span>2014</span>
+							            <em>Jun</em>
+						          	</div>
+						          	<div class="extra_wrapper">
+						            	<div class="col3"><a href="#">Convocatoria 1 </a></div>Rerna is sed feugiat. Cras pulvinar lorem sagi isallvestibulumnisi nec gravida. 
+						          	</div>
+						        </li>
+						        <li>
+						        	<div class="date">
+							            <strong>01</strong>
+							            <span>2014</span>
+							            <em>Jun</em>
+						          	</div>
+						          	<div class="extra_wrapper">
+						            	<div class="col3"><a href="#">Convocatoria 1 </a></div>Rerna is sed feugiat. Cras pulvinar lorem sagi isallvestibulumnisi nec gravida. 
+						          	</div>
+						        </li>
+						        <li>
+						        	<div class="date">
+							            <strong>01</strong>
+							            <span>2014</span>
+							            <em>Jun</em>
+						          	</div>
+						          	<div class="extra_wrapper">
+						            	<div class="col3"><a href="#">Convocatoria 1 </a></div>Rerna is sed feugiat. Cras pulvinar lorem sagi isallvestibulumnisi nec gravida. 
+						          	</div>
+						        </li>
+				      		</ul>
+			        		
+						</div>
+					</div>
+			        	</div>
+			      	</div>  			<div class="cl">&nbsp;</div>	
+			    <!-- Pie de Pagina-->
+				<div id="pie-pagina">
+					<div id="capa-pie">
+						<p>
+							<strong>Centro de Investigaciones, FCBI</strong>
+							<br>
+								Universidad de los llanos. Villavicencio, Colombia - Telefono 0000000
+							<br>
+								Correo electronico contacto@xxxx.com
+						</p>
+					</div>
+				</div>
+			</div>
+	</body>
+</html>	
