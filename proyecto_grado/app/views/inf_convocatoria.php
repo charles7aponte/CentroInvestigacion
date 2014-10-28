8eefdd8e7b2e3b95f27e46fadfb877b73fa06607
elizamt <elizamt_91@hotmail.com>
2014-10-28 16:30:52 -0500

    plantillas nuevas

---
+++ proyecto_grado/app/views/inf_convocatoria.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/> 
+  <link rel="stylesheet" type="text/css" href="estilo_infconvocatorias.css">
+</head>
+
+<body>
+	<div id="capa">
+		<div class="titulo-infconv" id="cuadro"> 
+			<h2>Titulo Convocatoria</h2>
+		</div>
+	  		<table class="table-infconvocatorias">
+                                   
+                <tbody>
+                    <tr>
+                        <th id="fila-principal">Numero Convocatoria</th>
+                        <td id="columna-principal" id="cuadro">512 MB</td>                    
+                    </tr>
+                    <tr>
+                        <th id="fila-principal">Estado</th>
+                        <td id="columna-principal" id="cuadro">50 GB</td>                      
+                    </tr>
+                    <tr id="descripcion">
+                        <th id="fila-principal">Descripci&oacute;n</th>
+                        <td id="columna-principal" id="cuadro">Unli hola como estas jajaja buu hola mitefdddddddddddddddd dddddddddddddddd ddddddddddd ffffffffffff dddddddddddddddddd dddddddddddddd f gggggggggggggggffflos pollitos </td>
+                    </tr>
+                    <tr id="dirigida" >
+                        <th id="fila-principal">Dirigida a</th>
+                        <td id="columna-principal" id="cuadro">19.90 $</td>                       
+                    </tr>
+                    <tr>
+                        <th id="fila-principal">Tel&eacute;fono Contacto</th>
+                        <td id="columna-principal" id="cuadro">19.90 $</td>                        
+                    </tr>
+                    <tr>
+                        <th id="fila-principal">P&aacute;gina Convocatoria</th>
+                        <td id="columna-principal" id="cuadro">19.90 $</td>                      
+                    </tr>
+
+                    <tr>
+                        <th id="fila-principal">Cuantia</th>
+                        <td id="columna-principal" id="cuadro">19.90 $</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <table class="table-infconvocatorias2">
+                                   
+                <tbody>
+                    <tr>
+                        <th id="fila-principal">Fecha Apertura</th>
+                        <td id="columna-principal" id="cuadro">512 MB</td>                    
+                    </tr>
+                    <tr>
+                        <th id="fila-principal">Fecha Cierre</th>
+                        <td id="columna-principal" id="cuadro">50 GB</td>                      
+                    </tr>
+                </tbody>
+            </table>
+
+            <table class="table-infconvocatorias1">
+                                   
+                <tbody>
+                    <tr>
+                        <th id="fila-principal">Archivo Convocatoria</th>
+                        <td id="columna-principal" id="cuadro">512 MB</td>                    
+                    </tr>
+                    <tr>
+                        <th id="fila-principal">URL convocatoria</th>
+                        <td id="columna-principal" id="cuadro">50 GB</td>                      
+                    </tr>
+                </tbody>
+            </table>
+
+    </div>
+</body>
+</html>
+
+<!-- <link rel="stylesheet" type="text/css" href="css/estilo_grupos.css">
+ <link rel="stylesheet" type="text/css" href="css/font-awesome.css">-->
