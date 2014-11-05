431f7b7950567f859b3ec815b5f327a701595d7e
andreakmargo08 <andreak_31@hotmail.com>
2014-11-05 17:58:23 -0500

    plantilla formulario

--- plantillas/public/formulario_grupos.php
+++ plantillas/public/formulario_grupos.php
@@ -7,11 +7,37 @@
 </head>
 <body>
 	<h2>Agregar un nuevo grupo</h2>
-	<label for="tipo-grupo">Tipo Grupo: </label>  
-      <select id="tipo-grupo">  
-         <option value="inv">Investigaci&oacute;n</option>
-         <option value="estudio">Estudio</option>    
-      </select> 
+
+	<div>  
+            <ul>  
+                <li>  
+                    <span class="required_notification">* Datos requeridos</span>  
+                </li>  
+                <li>  
+                    <label for="name">Nombre del grupo:</label>  
+                    <input type="text" required />  
+                </li>   
+                <li>
+                	<label for="tipo-grupo">Tipo Grupo: </label>  
+				    <select id="tipo-grupo">  
+				        <option value="inv">Investigaci&oacute;n</option>
+				        <option value="estudio">Estudio</option>    
+				    </select> 
+                </li>
+                <li>  
+                    <label for="coord">Coordinador:</label>  
+                    <input type="text" name="coordinador">   
+                </li>  
+                <li>  
+                    <label for="website">Sitio web:</label>  
+                    <input type="url" name="website" required pattern="(http|https)://.+" />   
+                </li>   
+                <li>  
+                    <button class="submit" type="submit">Enviar mensaje</button>  
+                </li>  
+            </ul>  
+        </div>  
+
 
 </body>
 </html>
