f438c2a65926f5437de110161fb1416fa5456a46
charles <charles7aponte@gmail.com>
2014-11-24 17:30:04 -0500

    cambio de prueba

--- proyecto_grado/app/views/administrador/panel_admin.blade.php
+++ proyecto_grado/app/views/administrador/panel_admin.blade.php
@@ -7,6 +7,7 @@ $dia_mes=$tiempo['mday'];
 $mes = $tiempo['mon']; 
 $year = $tiempo['year']; 
 
+
 switch ($dia){ 
 case "1": $dia_nombre="Lunes"; break; 
 case "2": $dia_nombre="Martes"; break; 
@@ -17,19 +18,20 @@ case "6": $dia_nombre="S&aacute;bado"; break;
 case "0": $dia_nombre="Domingo"; break; 
 } 
 switch($mes){ 
-case "1": $mes_nombre="Enero"; break; 
-case "2": $mes_nombre="Febrero"; break; 
-case "3": $mes_nombre="Marzo"; break; 
-case "4": $mes_nombre="Abril"; break; 
-case "5": $mes_nombre="Mayo"; break; 
-case "6": $mes_nombre="Junio"; break; 
-case "7": $mes_nombre="Julio"; break; 
-case "8": $mes_nombre="Agosto"; break; 
-case "9": $mes_nombre="Septiembre"; break; 
-case "10": $mes_nombre="Octubre"; break; 
-case "11": $mes_nombre="Noviembre"; break; 
-case "12": $mes_nombre="Diciembre"; break; 
+	case "1": $mes_nombre="Enero"; break; 
+	case "2": $mes_nombre="Febrero"; break; 
+	case "3": $mes_nombre="Marzo"; break; 
+	case "4": $mes_nombre="Abril"; break; 
+	case "5": $mes_nombre="Mayo"; break; 
+	case "6": $mes_nombre="Junio"; break; 
+	case "7": $mes_nombre="Julio"; break; 
+	case "8": $mes_nombre="Agosto"; break; 
+	case "9": $mes_nombre="Septiembre"; break; 
+	case "10": $mes_nombre="Octubre"; break; 
+	case "11": $mes_nombre="Noviembre"; break; 
+	case "12": $mes_nombre="Diciembre"; break; 
 } 
+
 ?>
 
 <!doctype html>
