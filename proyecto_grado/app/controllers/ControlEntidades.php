35304c26634d9ba9d5ca148a9ead65818930b5d9
elizamt <elizamt_91@hotmail.com>
2014-12-04 13:42:41 -0500

    formularios con base

--- proyecto_grado/app/controllers/ControlEntidades.php
+++ proyecto_grado/app/controllers/ControlEntidades.php
@@ -18,23 +18,27 @@ class ControlEntidades extends Controller {
 		$pagina_entidad=Input::get('pagina-entidad');
 		$telefono_entidad=Input::get('telefono-entidad');
 		$celular_entidad=Input::get('celular-entidad');
+
+		var_dump(Input::all());
 	
+//$entidad=new InvEntidades();
+
 
 		/*objeto del modelo*/
-		$entidad=new InvEntidad();
+		//$entidad=new InvEntidad();
 		
-		$entidad->nit_entidad=$nit;
-		$entidad->nombre_empresa=$entidad_nombre;
-		$entidad->descripcion=$descripcion;
+		//$entidad->nit_empresa=$nit;
+		/*$entidad->nombre_empresa=$entidad_nombre;
+		$entidad->descripcion_empresa=$descripcion;
 		$entidad->email=$email_entidad;
-		$entidad->paginaweb=$pagina_entidad;
+		$entidad->pagina_web=$pagina_entidad;
 		$entidad->representante_legal=$representante;
 		$entidad->telefono=$telefono_entidad;
 		$entidad->celular=$celular_entidad;
 		$entidad->tipo_empresa=$tipo;
+		*/
 
-		$entidad->save();
-	
-	}
+		//$entidad->save();
+		}
 
 }
