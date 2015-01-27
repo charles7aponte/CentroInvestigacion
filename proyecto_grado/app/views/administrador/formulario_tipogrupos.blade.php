1a47318473bbce5a8e2118a58a6a3873df306f52
charles <charles7aponte@gmail.com>
2015-01-27 10:46:48 -0500

    pq descripcion

--- proyecto_grado/app/views/administrador/formulario_tipogrupos.blade.php
+++ proyecto_grado/app/views/administrador/formulario_tipogrupos.blade.php
@@ -33,6 +33,18 @@
 </div>
 
 
+
+
+<div  id="gato" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      ...
+    </div>
+  </div>
+</div>
+
+
+
 <div>  
     <form id="form-tipogrupo" autocomplete="on"   action="{{URL::to('creacion/formulariotipogrupos')}}" method="post">
 
