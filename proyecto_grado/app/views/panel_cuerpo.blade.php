a84b70d48a53c65f213b2951667c11b166c7d4cd
charles <charles7aponte@gmail.com>
2015-04-08 10:25:17 -0500

    carusel index basico

--- proyecto_grado/app/views/panel_cuerpo.blade.php
+++ proyecto_grado/app/views/panel_cuerpo.blade.php
@@ -57,9 +57,9 @@
 		@show
 		
 	    <!-- scripts -->
-	    <script type="text/javascript" src="{{URL::to('js/jquery.js')}}"></script>
+<!--	    <script type="text/javascript" src="{{URL::to('js/jquery.js')}}"></script>
 
-	<!--	<script src="{{URL::to('js/jquery-1.5.2.min.js')}}" type="text/javascript"></script>
+		<script src="{{URL::to('js/jquery-1.5.2.min.js')}}" type="text/javascript"></script>
 -->
 		<script src="{{URL::to('js/jquery-1.11.1.min.js')}}" type="text/javascript"></script>
 	    <script type="text/javascript" src="{{URL::to('js/scripts.js')}}"></script>
